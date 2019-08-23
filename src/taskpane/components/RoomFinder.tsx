cd1829efebbafcb9350fbe0b6c64f1579cc004e6
Ryan Townsend <rtownsend@aais.com>
2019-08-22 21:54:48 -0500

    promise chain for start and end time
    Office APi requires async retrieval of field date, using promise all to resolve both and then get the times --> these will feed the API request for available rooms.

--- src/taskpane/components/RoomFinder.tsx
+++ src/taskpane/components/RoomFinder.tsx
@@ -1,22 +1,58 @@
 import * as React from 'react';
+import { Button } from 'office-ui-fabric-react';
 
 export interface AppProps {
 }
 
 export interface AppState { 
+  startTime: Date;
+  endTime: Date;
 }
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      startTime: null,
+      endTime: null,
+    };
   }
+
 
   componentDidMount() {
   }
 
+  makePromise = function (itemField) {
+    return new Promise(function(resolve, reject) {
+      itemField.getAsync(function (asyncResult) {
+        if (asyncResult.status.toString === "failed") {
+          reject(asyncResult.error.message);
+        }
+        else {
+          resolve(asyncResult.value);
+        }
+      });
+    });
+  }
+
+  click = async () => {
+
+    var item = Office.context.mailbox.item;
+    Promise.all([this.makePromise(item.start), this.makePromise(item.end)])
+      .then(function(values) {
+        console.log(values);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  
   render() {
     return (
-      <div>Room finder will go here</div>
-    );
+      <div>
+        <Button className='ms-welcome__action'  onClick={this.click}>Refresh</Button>
+        <div>Here is what I pulled of invite: {JSON.stringify(this.state)} </div>
+      </div>
+);
   }
 }
