97b311dec970c61fe796b8aef1575463faef4ccd
Ryan Townsend <rtownsend@aais.com>
2019-08-22 23:15:56 -0500

    added search and toggle to room list
    stubbed in UI components for searching room by name and toggling show only available rooms

--- src/taskpane/components/RoomFinder.tsx
+++ src/taskpane/components/RoomFinder.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Button } from 'office-ui-fabric-react';
+import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
 export interface AppProps {
 }
@@ -50,6 +52,26 @@ export default class App extends React.Component<AppProps, AppState> {
   render() {
     return (
       <div>
+        <div className="ms-SearchBoxExample">
+          <SearchBox
+            placeholder="Search by Ad Astra room name"
+            onSearch={newValue => console.log('value is ' + newValue)}
+            onFocus={() => console.log('onFocus called')}
+            onBlur={() => console.log('onBlur called')}
+            onChange={() => console.log('onChange called')}
+          />
+        </div>
+        <Toggle
+          defaultChecked={true}
+          label=""
+          onText="Only available rooms"
+          offText="Only available rooms"
+          onFocus={() => console.log('onFocus called')}
+          onBlur={() => console.log('onBlur called')}
+          onChange={() => console.log('onChange called')}
+        />
+        <div>Place holder for scrolling list of compound buttons (rooms)</div>
+
         <Button className='ms-welcome__action'  onClick={this.click}>Refresh</Button>
         <div>Here is what I pulled of invite: {JSON.stringify(this.state)} </div>
       </div>
